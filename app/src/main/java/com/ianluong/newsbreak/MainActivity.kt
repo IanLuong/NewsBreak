fc721c95bdf99d8beab25ef90080b0b941013876
Ian <ianluong@hotmail.co.uk>
2021-04-20 12:00:25 +0100

    Add startup activity functions

--- app/src/main/java/com/ianluong/newsbreak/MainActivity.kt
+++ app/src/main/java/com/ianluong/newsbreak/MainActivity.kt
@@ -1,23 +1,38 @@
 package com.ianluong.newsbreak
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var openButton: Button
+    lateinit var openButton: Button
+    lateinit var urlEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        openButton = findViewById(R.id.open_button)
+        urlEditText = findViewById(R.id.url_edit_text)
 
+        openButton = findViewById(R.id.open_button)
         openButton.setOnClickListener {
-            Toast.makeText(this, R.string.open_button_toast, Toast.LENGTH_SHORT).show()
+            val website = Uri.parse(urlEditText.text.toString())
+            val intent = Intent(Intent.ACTION_VIEW, website)
+            try {
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(this, "URL not found\n Use https://...", Toast.LENGTH_SHORT).show()
+            }
         }
+
     }
+
+
 
 }
