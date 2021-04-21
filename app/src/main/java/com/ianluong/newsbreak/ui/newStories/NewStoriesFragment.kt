8d294e873d4758d2343faa0e371e7a178deda1fa
Ian <ianluong@hotmail.co.uk>
2021-04-22 00:17:18 +0100

    Implement new articles fragment recyclerview binding

--- app/src/main/java/com/ianluong/newsbreak/ui/newStories/NewStoriesFragment.kt
+++ app/src/main/java/com/ianluong/newsbreak/ui/newStories/NewStoriesFragment.kt
@@ -8,28 +8,24 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ianluong.newsbreak.Article
+import com.ianluong.newsbreak.HomeActivity
 import com.ianluong.newsbreak.R
 
-private const val TAG = "CrimeListFragment"
+private const val TAG = "NewStoriesFragment"
 
 class NewStoriesFragment : Fragment() {
 
     private lateinit var newStoriesViewModel: NewStoriesViewModel
+    private lateinit var articleRecyclerView: RecyclerView
+    private var adapter: NewStoryHolder? = null
 
-    private lateinit var article: Article
-    private lateinit var articleTitle: TextView
-    private lateinit var articleDescription: TextView
-    private lateinit var articleImage: ImageView
-    private lateinit var followButton: Button
-    private lateinit var readMoreButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,31 +38,22 @@ class NewStoriesFragment : Fragment() {
     ): View? {
         newStoriesViewModel =
             ViewModelProvider(this).get(NewStoriesViewModel::class.java)
-        Log.d(TAG, "Total crimes ${newStoriesViewModel.articles.size}")
+        Log.d(TAG, "Total articles ${newStoriesViewModel.articles.size}")
 
         val root = inflater.inflate(R.layout.fragment_new_stories, container, false)
 
-        articleTitle = root.findViewById(R.id.article_title_textview)
-        articleDescription = root.findViewById(R.id.article_description_textview)
-        articleImage = root.findViewById(R.id.article_image_imageview)
-        followButton = root.findViewById(R.id.follow_button)
-        readMoreButton = root.findViewById(R.id.read_more_button)
+        articleRecyclerView = root.findViewById(R.id.article_recycler_view)
+        articleRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        updateUI()
 
         return root
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        readMoreButton.setOnClickListener() {
-            val website = Uri.parse("https://google.com")
-            val intent = Intent(Intent.ACTION_VIEW, website)
-            try {
-                startActivity(intent)
-            } catch (e: Exception) {
-                Log.d("ArticleFragment.kt", "Not a URL")
-            }
-        }
+    private fun updateUI() {
+        val articles = newStoriesViewModel.articles
+        val adapter = NewStoryAdapter(articles)
+        articleRecyclerView.adapter = adapter
     }
 
     companion object {
@@ -74,6 +61,56 @@ class NewStoriesFragment : Fragment() {
             return NewStoriesFragment()
         }
     }
-}
+
+    private inner class NewStoryHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private lateinit var article: Article
+
+        private val articleTitle: TextView = itemView.findViewById(R.id.article_title_textview)
+        private val articleDescription: TextView = itemView.findViewById(R.id.article_description_textview)
+        private val articleImage: ImageView = itemView.findViewById(R.id.article_image_imageview)
+        private val followButton: Button = itemView.findViewById(R.id.follow_button)
+        private val readMoreButton: Button = itemView.findViewById(R.id.read_more_button)
+
+        fun bind(article: Article) {
+            articleTitle.text = article.title
+            articleDescription.text = article.description
+            //articleImage = article.title TODO add image binding functionality
+            //followButton.text = article.title TODO add follow button functionality
+            setReadMoreButtonListener(readMoreButton, article.url)
+        }
+
+        fun setReadMoreButtonListener(button: Button, url: String?) {
+            button.setOnClickListener() {
+                val website = Uri.parse(url) ?: null
+                if (website != null) {
+                    val intent = Intent(Intent.ACTION_VIEW, website)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(context, "Article not found", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
+
+    private inner class NewStoryAdapter(var articles: List<Article>) :
+        RecyclerView.Adapter<NewStoryHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewStoryHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_story, parent, false)
+            return NewStoryHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: NewStoryHolder, position: Int) {
+            val article = articles[position]
+            holder.bind(article)
+        }
+
+        override fun getItemCount(): Int {
+            return articles.size
+        }
+
+        }
+    }
 
 
--- app/src/main/java/com/ianluong/newsbreak/ui/newStories/NewStoriesViewModel.kt
+++ app/src/main/java/com/ianluong/newsbreak/ui/newStories/NewStoriesViewModel.kt
@@ -11,13 +11,13 @@ class NewStoriesViewModel : ViewModel() {
     val articles = mutableListOf<Article>()
 
     init {
-        for(i in 0..19) {
+        for(i in 0..99) {
             val article = Article()
             article.title = "Article number " + i
-            var description = "Description " + i
-            var url = "https://www.bbc.co.uk/news/uk-politics-56832486"
-            var urlToImage = "https://ichef.bbci.co.uk/news/976/cpsprodpb/CAAB/production/_118138815_dyson2_johnson_texts2x640-nc.png"
-            var publishedAt : Date? = Date("11/11/11")
+            article.description = "Description number " + i
+            article.url = "https://www.bbc.co.uk/news/uk-politics-56832486"
+            article.urlToImage = "https://ichef.bbci.co.uk/news/976/cpsprodpb/CAAB/production/_118138815_dyson2_johnson_texts2x640-nc.png"
+            article.publishedAt = Date("11/11/11")
             articles.add(article)
         }
     }
