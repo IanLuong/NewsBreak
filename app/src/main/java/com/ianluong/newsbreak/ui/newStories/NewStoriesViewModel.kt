533ad30a43380aaf672dbc0991dc449169a82dab
Ian <ianluong@hotmail.co.uk>
2021-07-17 19:48:58 +0100

    Add Article Interceptor and refactor api key

--- app/src/main/java/com/ianluong/newsbreak/ui/newStories/NewStoriesViewModel.kt
+++ app/src/main/java/com/ianluong/newsbreak/ui/newStories/NewStoriesViewModel.kt
@@ -11,15 +11,21 @@ import java.util.*
 
 class NewStoriesViewModel : ViewModel() {
 
-    val articlesLiveData: LiveData<List<Article>> = NewsFetcher().fetchNews("UKHeadlines")
+    val articlesLiveData: LiveData<List<Article>>
 
     private val articleRepository: ArticleRepository = ArticleRepository.get()
     private val articleIdLiveData = MutableLiveData<UUID>()
+
+
 
     var articleLiveData: LiveData<Article?> =
         Transformations.switchMap(articleIdLiveData) {
             articleRepository.getArticle(it)
         }
+
+    init {
+        articlesLiveData = NewsFetcher().fetchNews("UKHeadlines") //UKHeadlines is the default on startup
+    }
 
     fun loadArticle(articleId: UUID) {
         articleIdLiveData.value = articleId
