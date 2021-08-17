80566dffc5a83047cf101daec19ffbd0bb3065da
Ian <ianluong@hotmail.co.uk>
2021-08-17 12:42:10 +0100

    Rename and remove old main activity

--- app/src/main/java/com/ianluong/newsbreak/MainActivity.kt
+++ app/src/main/java/com/ianluong/newsbreak/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
