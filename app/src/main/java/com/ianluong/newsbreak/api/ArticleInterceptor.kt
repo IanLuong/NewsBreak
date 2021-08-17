a2f4c1d2ffc9fa396205108700df216fcf5c7909
IanLuong <ianluong@hotmail.co.uk>
2021-08-17 18:31:34 +0100

    Fix API Key

--- app/src/main/java/com/ianluong/newsbreak/api/ArticleInterceptor.kt
+++ app/src/main/java/com/ianluong/newsbreak/api/ArticleInterceptor.kt
@@ -6,12 +6,14 @@ import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
+private const val API_KEY = "34acee49c2054580bc2e8aa1fe0dfcb4"
+
 class ArticleInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest: Request = chain.request()
 
         val newUrl: HttpUrl = originalRequest.url().newBuilder()
-            .addQueryParameter("apiKey", BuildConfig.NAPI_KEY)
+            .addQueryParameter("apiKey", API_KEY)
             .build()
 
         val newRequest: Request = originalRequest.newBuilder().url(newUrl).build()
